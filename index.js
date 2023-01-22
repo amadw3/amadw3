501a8a8a461710c7bd80c171075c5d7681bb99b5
amadw3 <123281007+amadw3@users.noreply.github.com>
2023-01-22 06:19:35 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 100);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+  document.getElementById("mainBody").style.display = "block";
+}
